85f4c2aed26a020161832984ae1d4e26756990ad
Dwiki Nuri Dhuha <dwiki.nuridhuha@gmail.com>
2025-05-31 08:59:27 +0700

    length array

--- array.go
+++ array.go
@@ -13,7 +13,15 @@ func main() {
 		"one", "two", "three",
 	}
 
+	var lengthNames = len(names)
+	var lengthNumbers = len(numbers)
+	var lengthMatrix = len(matrix)
+
 	fmt.Println("Names:", names)
 	fmt.Println("Numbers:", numbers)
 	fmt.Println("Matrix:", matrix)
+
+	fmt.Println("Length of names array:", lengthNames)
+	fmt.Println("Length of numbers array:", lengthNumbers)
+	fmt.Println("Length of matrix array:", lengthMatrix)
 }
